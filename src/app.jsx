47915103b19519b3078f3ea0e4cf50e8733a4fd7
Dj <rodriguezdanilo88@gmail.com>
2018-03-02 19:42:33 -0800

    working axios

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        topspots: []
+    }
+  axios
+    .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+    .then(response => response.data)
+    .then(topspots => this.setState({ topspots }));
+  }
+
   render() {
     return (
-      <div className='App'></div>
+      <div className='App'>
+      <pre>{ JSON.stringify(this.state.topspots, null, 2) }</pre>
+        <div className='container mt-5'>
+          <div className='card border border-dark'>
+            <div className='card-body'>
+              <h1>
+                San Diego Top Spots
+              </h1>
+              <h5>
+                A list of the top 30 places to see in San Diego, California
+              </h5>  
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
